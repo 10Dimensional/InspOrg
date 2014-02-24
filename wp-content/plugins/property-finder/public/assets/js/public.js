d9fb29454a50ca3d3d58142d3035ef0deeb2bc37
Michael Aguiar <mike@aliasproject.com>
2014-02-24 13:33:15 -0700

    IE8 fix

--- wp-content/plugins/property-finder/public/assets/js/public.js
+++ wp-content/plugins/property-finder/public/assets/js/public.js
@@ -34,9 +34,7 @@ var ui = '';
 /*             map.setZoomRange(2, 4); */
             var mapgroup = L.layerGroup().addTo(map);
             
-            console.log(builder_layers);
             for (var b in builder_layers) {
-                console.log(b);
                 if (builder_layers[b]) {
                     //addLayer(L.tileLayer(builder_layers[b]), b, 1);
                     mapgroup.addLayer(L.tileLayer(builder_layers[b]), b, 1);
@@ -71,7 +69,6 @@ var ui = '';
             	    for (var b in builder_layers) {
                         
                         if (builder_layers[response.builders[b]]) {
-                            console.log('t');
                             addLayer(L.tileLayer(builder_layers[b]), b, 1);
                             //mapgroup.addLayer(L.tileLayer(builder_layers[response.builders[b]]), b, 1);
                         }
@@ -126,7 +123,6 @@ var ui = '';
                     	    $('#requestInfo .step1').hide();
                     	    var model_list = '';
                     	    for (var x = 0; x <= response.interested_models.length - 1; x++) {
-                    	        console.log(response.interested_models[x]);
                         	    model_list += '<li><a href="'+response.interested_models[x].url+'" target="_blank">'+response.interested_models[x].builder+': '+response.interested_models[x].model+'</a></li>';
                     	    }
                     	    
@@ -168,7 +164,6 @@ function addLayer(layer, name, zIndex) {
     // and off.
     //var item = document.createElement('li');
     var link = document.getElementById(name);
-    console.log(link);
 //Prevents the layers from loading until they're clicked on the selector
  
     link.onclick = function(e) {
