ae3e0540905bd20c8c8521ef6f31e3d01605ebfd
schristoff <davidherbold@Davids-Mac-mini.local>
2014-02-24 10:35:20 -0700

    typo

--- wp-content/themes/inspirada/page.php
+++ wp-content/themes/inspirada/page.php
@@ -14,7 +14,7 @@
       <link media="all" rel="stylesheet" href="<?php bloginfo('template_url') ?>/css/all.css">
       <link media="all" rel="stylesheet" href="<?php bloginfo('template_url') ?>/css/jcf.css">
       <script type="text/javascript" src="<?php bloginfo('template_url') ?>/js/jquery-1.8.3.min.js"></script>
-      <script type="text/javascript" src="<?php bloginfo('template_url') ?>/js/modernizer.js"></script>
+      <script type="text/javascript" src="<?php bloginfo('template_url') ?>/js/modernizr.js"></script>
       
       <!--[if IE]><script type="text/javascript" src="js/ie.js"></script><![endif]-->
       <!--[if lt IE 9]> 
