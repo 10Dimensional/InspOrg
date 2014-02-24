b0a83576e5d49bb89d394e1a399565bed0aca559
schristoff <davidherbold@Davids-Mac-mini.local>
2014-02-24 10:42:37 -0700

    adding modernizr everywhere

--- wp-content/themes/inspirada/gallery-page.php
+++ wp-content/themes/inspirada/gallery-page.php
@@ -15,6 +15,8 @@
 	<link media="all" rel="stylesheet" href="<?php bloginfo('template_url') ?>/css/jcf.css">
 	<!--[if IE]><script type="text/javascript" src="js/ie.js"></script><![endif]-->
 	<!--[if lt IE 9]><link rel="stylesheet" href="css/ie.css" media="screen"/><![endif]-->
+	      <script type="text/javascript" src="<?php bloginfo('template_url') ?>/js/modernizr.js"></script>
+
 <?php wp_head() ?></head>
 <body>
 	<div id="wrapper">
