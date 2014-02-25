0ba3ea2751c6262adcf654bb81c628a7a277110d
schristoff <davidherbold@Davids-Mac-mini.local>
2014-02-25 14:34:41 -0700

    zoom in on contact

--- wp-content/themes/inspirada/contact-page.php
+++ wp-content/themes/inspirada/contact-page.php
@@ -56,7 +56,10 @@
   </style>
 <div id='map' style="height: 614px; width: 1003px; margin: 0 auto;"></div>
 <script>
-var map = L.mapbox.map('map');
+var map = L.map('map', {
+	minZoom: 2,
+	maxZoom: 5
+	});
 var baselayer = L.tileLayer('http://166.78.0.133:8888/v2/base/{z}/{x}/{y}.png').addTo(map);
 map.setView([-77, 22.763671875], 4);
 map.touchZoom.disable();
