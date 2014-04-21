920aae412ec64e3c5581328af6e9fb7158229b37
Michael Aguiar <mike@aliasproject.com>
2014-04-21 16:04:46 -0700

    bug fixes

--- wp-content/plugins/property-finder/public/ajax.php
+++ wp-content/plugins/property-finder/public/ajax.php
@@ -258,11 +258,14 @@
         // set up basic connection
         $conn_id = ftp_connect('64.94.4.105');
         if (@ftp_login($conn_id, 'ftp-inspirada', 'M@st3rp1@n')) {
-            if (ftp_put($conn_id, time().'.xml', $file, FTP_ASCII)) {
+            print_r(ftp_put($conn_id, time().'.xml', $file, FTP_ASCII));
+            /*
+if (ftp_put($conn_id, time().'.xml', $file, FTP_ASCII)) {
                 $msg = "Successfully uploaded $file\n";
             } else {
                 $msg = "There was a problem while uploading $file\n";
             }
+*/
         } else {
             echo "Couldn't connect as $ftp_user\n";
        } 
