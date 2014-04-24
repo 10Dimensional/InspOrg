71ce6044ffa9316febfd658153604a359bd439e7
Michael Aguiar <mike@aliasproject.com>
2014-04-24 16:05:52 -0700

    testing

--- wp-content/themes/inspirada/fullwidth_thanks_alt.php
+++ wp-content/themes/inspirada/fullwidth_thanks_alt.php
@@ -6,6 +6,7 @@
                 $price = explode('to', str_replace(',', '', str_replace('$', '', $_GET['price_range'])));
                 $sqft = explode('to', str_replace(',', '', str_replace('sq ft', '', $_GET['sqft'])));
                 $bedrooms = $_GET['bedrooms'];
+                echo 'bedrooms: ';
                 print_r($bedrooms);
                 
                 $property_id = array();
