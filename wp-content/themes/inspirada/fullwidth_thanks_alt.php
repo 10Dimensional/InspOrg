fd59871807a581ee64200afc6c0b5fa03b8e29b6
Michael Aguiar <mike@aliasproject.com>
2014-04-24 16:16:26 -0700

    bug fixes

--- wp-content/themes/inspirada/fullwidth_thanks_alt.php
+++ wp-content/themes/inspirada/fullwidth_thanks_alt.php
@@ -19,7 +19,7 @@
                 $sq_ft_max = ($_GET['sqft'] === 'over 5,000 sq ft') ? 99999999999 : $sqft[1];
                 
 
-                $where_clause = 'WHERE beds_min >= '.$bedrooms.' AND sq_ft >= '.$sq_ft_min.' AND sq_ft <= '.$sq_ft_max;    
+                $where_clause = 'WHERE beds_min >= "'.$bedrooms.'" AND sq_ft >= '.$sq_ft_min.' AND sq_ft <= '.$sq_ft_max;    
                 
                 if ($builder) {
                     $where_clause .= " AND builder IN ('".implode("','",$builder).'\')';
