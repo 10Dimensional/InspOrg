799270e9fea13f08c0a2fac28b8418366cdf4344
Michael Aguiar <mike@aliasproject.com>
2014-05-16 16:43:36 -0700

    bug fixes

--- wp-content/themes/inspirada/functions.php
+++ wp-content/themes/inspirada/functions.php
@@ -195,8 +195,8 @@ function generate_xml_email_kb_main($firstName, $lastName, $email, $phone, $comm
     if ($_SERVER['HTTP_HOST'] !== 'www.inspirada.com') return;
     require_once "Mail.php";
     require_once "Mail/mime.php";
-    //$to = 'inspirada@kbhome.com';
-    $to = 'mike@aliasproject.com';
+    $to = 'inspirada@kbhome.com';
+    
     $xml = '<?xml version="1.0" encoding="UTF-8" ?>';
     $xml .= '<hsleads>'.PHP_EOL;
     $xml .= '<lead>'.PHP_EOL;
